087528dd1fd205f4d6bfca2e5aec28346a165a08
Scott Wattenbarger <scottwattenbarger@gmail.com>
2022-07-26 21:01:42 -0700

    adds streaming function to see what streaming services the movie is on

--- assets/script.js
+++ assets/script.js
@@ -51,47 +51,67 @@ function movieSelected(id){
 
 function movieDetail(){
     let movieId = sessionStorage.getItem('movieId');
-
+    
     fetch(`http://www.omdbapi.com/?i=${movieId}&apikey=99e26c0f`)
     .then(function(response){
-        return response.json()
-        
-        .then(function(data){
+      return response.json()
+      
+      .then(function(data){
         console.log(data);
         let movie = data;
         let output = `
         <div class="row">
-          <div class="col-md-4">
-            <img src="${movie.Poster}" class="thumbnail">
-          </div>
-          <div class="col-md-8">
-            <h2>${movie.Title}</h2>
-            <ul class="list-group">
-              <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
-              <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
-              <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
-              <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
-              <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+        <div class="col-md-4">
+        <img src="${movie.Poster}" class="thumbnail">
+        </div>
+        <div class="col-md-8">
+        <h2>${movie.Title}</h2>
+        <ul class="list-group">
+        <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+        <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+        <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+        <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+        <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
               <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
               <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
-            </ul>
-          </div>
-        </div>
-        <div class="row">
-          <div class="well">
-            <h3>Plot</h3>
-            ${movie.Plot}
-            <hr>
-            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-            <a href="index.html" class="btn btn-default">Go Back To Search</a>
-          </div>
-        </div>
-      `;
-
-      $('#movie').html(output);
-        })
-        .catch(function(error){
-            console.log(error)
-        });
-    })
-}
+              </ul>
+              </div>
+              </div>
+              <div class="row">
+              <div class="well">
+              <h3>Plot</h3>
+              ${movie.Plot}
+              <hr>
+              <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+              <a href="index.html" class="btn btn-default">Go Back To Search</a>
+              </div>
+              </div>
+              `;
+              
+              $('#movie').html(output);
+              sessionStorage.setItem('title', movie.Title);
+              streaming()
+            })
+            .catch(function(error){
+              console.log(error)
+            });
+            
+          })
+        }
+        
+        function streaming(){
+          let title = sessionStorage.getItem('title');
+          console.log(title);
+          const options = {
+            method: 'GET',
+            headers: {
+              'X-RapidAPI-Key': 'd3d4c5f317mshee3f91b68ed1105p1081f6jsn6048356913dc',
+              'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+            }
+          };
+          
+          fetch(`https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&keyword=${title}&output_language=en&language=en`, options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err))
+        }
