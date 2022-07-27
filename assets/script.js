492d0f9be943fa1d52e26c8f37d126c256236988
Scott Wattenbarger <scottwattenbarger@gmail.com>
2022-07-26 19:00:28 -0700

    adds a card with the api details

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,51 @@
-    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=573fd5a2`)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data){
-            console.log(data);
-        })
+$(document).ready(() => {
+    $('#searchForm').on('submit', (e) => {
+        console.log($('#searchText').val)
+      let searchText = $('#searchText').val();
+      getMovies(searchText);
+      e.preventDefault();
+    });
+  });
+  
+  function getMovies(searchText){
+   fetch(`http://www.omdbapi.com/?s=${searchText}&apikey=99e26c0f`)
+   .then(function(response){
+    return response.json()
+    
+    .then(function(data){
+    console.log(data);
+    let movies = data.Search;
+    let output = '';
+    $.each(movies, (index, movie) => {
+        output += `
+        <div class="row">
+            <div class="col s12 m7>
+                <div class="card">
+                    <div class="card-image">
+                        <img src="${movie.Poster}">
+                        <span class="card-title>${movie.Title}</span>
+                    </div>
+                
+                    <div class="card-action">
+                        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                    </div>
+                </div>    
+            </div>
+        </div>
+      `;
+    });
+
+    $('#movies').html(output);
+    })
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+}
+
+function movieSelected(id){
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return false;
+}
+
