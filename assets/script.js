0ec591e53e73f843b2f65ca1c5bce7f8d2152e4b
Scott Wattenbarger <scottwattenbarger@gmail.com>
2022-07-29 09:17:39 -0700

    changes format to flex in css

--- assets/script.js
+++ assets/script.js
@@ -18,18 +18,17 @@ $(document).ready(() => {
     let output = '';
     $.each(movies, (index, movie) => {
         output += `
-        <div class="row">
-            <div class="col s4 m7 center-align>
-                <div class="card">
-                    <div class="card-image">
-                        <img src="${movie.Poster}">
-                        <span class="card-title>${movie.Title}</span>
-                        <div class="card-action">
-                            <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
-                        </div>
-                    </div>
-                
-                </div>    
+        <div class="col s12 m6">
+            <h2 class="header">${movie.Title}</span>
+            <div class="card vertical">
+                <div class="card-image">
+                    <img src="${movie.Poster}">
+                </div>
+                <div class="card-stacked"> 
+                    <div class="card-action">
+                        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                    </div>   
+                </div>
             </div>
         </div>
       `;
@@ -83,12 +82,11 @@ function movieDetail(){
               ${movie.Plot}
               <hr>
               <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-              <a href="https://www.youtube.com/watch?v=${response.video}" target="_blank" class="btn btn-primary">View Trailer</a>
-              <a href="index.html" class="btn btn-default">Return Home</a>
+              <a href="index.html" class="btn btn-default">Home Page</a>
               </div>
               </div>
               `;
-              console.log(response.video);
+              
               $('#movie').html(output);
               sessionStorage.setItem('title', movie.Title);
               streaming()
@@ -104,23 +102,25 @@ function movieDetail(){
           let title = sessionStorage.getItem('title');
           let movieId = sessionStorage.getItem('movieId')
           console.log(title);
-          const options = {
-            method: 'GET',
-            headers: {
-              'X-RapidAPI-Key': 'd3d4c5f317mshee3f91b68ed1105p1081f6jsn6048356913dc',
-              'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-            }
-          };
+          // const options = {
+          //   method: 'GET',
+          //   headers: {
+          //     'X-RapidAPI-Key': 'd3d4c5f317mshee3f91b68ed1105p1081f6jsn6048356913dc',
+          //     'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+          //   }
+          // };
           
-            
+          // fetch(`https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&keyword=${title}&output_language=en&language=en`, options)
+          //   .then(response => response.json())
+          //   .then(response =>{
 
-            // const options = {
-            //   method: 'GET',
-            //   headers: {
-            //     'X-RapidAPI-Key': 'ab437ecc54msh017cc446f57b0c5p1b3250jsn88cc1e48f2e2',
-            //     'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-            //   }
-            // };
+            const options = {
+              method: 'GET',
+              headers: {
+                'X-RapidAPI-Key': 'ab437ecc54msh017cc446f57b0c5p1b3250jsn88cc1e48f2e2',
+                'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+              }
+            };
             
             fetch(`https://streaming-availability.p.rapidapi.com/get/basic?country=us&imdb_id=${movieId}&output_language=en`, options)
               .then(response => response.json())
@@ -130,11 +130,11 @@ function movieDetail(){
               let streaming = response.streamingInfo;
               let streamingName = '';
               let streamingLink = ''
-              console.log(streaming);
               if(streaming.disney){
                 streamingName = 'Disney Plus';
                 streamingLink = streaming.disney.us.link;
                 
+                console.log(streaming);
               }else if(streaming.netflix){
                 streamingName = 'Netflix';
                 streamingLink = streaming.netflix.us.link;
@@ -155,6 +155,7 @@ function movieDetail(){
                 streamingName = 'Starz';
                 streamingLink = streaming.starz.us.link;
                       
+                $('#stream').html(output)
               }else if(streaming.showtime){
                 streamingName = 'Showtime';
                 streamingLink = streaming.showtime.us.link;
@@ -163,9 +164,7 @@ function movieDetail(){
                 streamingName = 'Apple Tv';
                 streamingLink = streaming.apple.us.link;
             }else{
-                streamingName = 'Not on a streaming service';
-    
-                
+                streamingName = 'Not on a streaming service.'
             }        
             
             if(streamingLink !== ''){
@@ -182,23 +181,21 @@ function movieDetail(){
             `;
             
             $('#stream').html(output)
-            }else {
-              let output = `
-            <div class="row">
-              <div class="col-md-8">
-                <h2>What streaming site?</h2>
-                <ul class="list-group">
-                  <li class="list-group-item"><strong>Stream site:</strong>  ${streamingName}</a></li>
-                </ul>
-              </div>
-            </div>
+            }else{
+                let output = `
+                <div class="row">
+                  <div class="col-md-8">
+                    <h2>What streaming site?</h2>
+                    <ul class="list-group">
+                      <li class="list-group-item"><strong>Stream site:</strong>  ${streamingName}</a></li>
+                    </ul>
+                  </div>
+                </div>
+                    
+                `;
                 
-            `;
-            
-            $('#stream').html(output)
-          };
-          sessionStorage.setItem('trailer', response.video)
-
+                $('#stream').html(output)
+            }
             })
             
             .catch(err => console.error(err))
